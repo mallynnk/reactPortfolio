b129c6ebcd38ba8018fc29afa3d82bc7e8cb2a53
mallynnk <mallory.korpics@gmail.com>
2020-11-02 12:26:25 -0600

    added profile pic & image folders

---
+++ src/components/Resume/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Resume() {
+
+    return (
+        <section id="resume">
+            Download my full <a href="https://github.com/mallynnk/reactportfolio/raw/master/src/assets/Resume_Willeke.pdf" class="link">resume</a>
+        </section>
+    );
+
+}
+
+export default Resume;
