57be9b3be5e24837c48bcc54036306617b12edd8
mallynnk <mallory.korpics@gmail.com>
2020-11-15 18:34:13 -0600

    queries

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -4,43 +4,6 @@ import Card from 'react-bootstrap/Card'
 
 function Contact() {
 
-    // const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    // const { name, email, message } = formState;
-    // const [errorMessage, setErrorMessage] = useState('');
-
-    // function handleChange(e) {
-
-    //     if (e.target.name === 'email') {
-
-    //         const isValid = validateEmail(e.target.value);
-
-    //         if (!isValid) {
-    //             setErrorMessage('Your email is invalid.');
-    //         } else {
-
-    //             if (!e.target.value.length) {
-    //                 setErrorMessage(`${e.target.name} is required.`);
-    //             } else {
-    //                 setErrorMessage('');
-    //             }
-    //         }
-
-    //     }
-
-    //     console.log('errorMessage', errorMessage);
-
-    //     if (!errorMessage) {
-    //         setFormState({ ...formState, [e.target.name]: e.target.value });
-    //     }
-
-    // }
-
-    // function handleSubmit(e) {
-    //     e.preventDefault();
-    //     console.log(formState);
-    // }
-
-
     var style = {
         fontSize: "4rem",
         textAlign: "center",
@@ -48,7 +11,7 @@ function Contact() {
     }
 
     return (
-        <div style={{padding: '20rem'}} className="contact">
+        <div className="contact">
         <h1 style={style}>get in touch</h1>
         <Card style={{color: '#5E7364', padding:'5rem', width: '18rem ', textAlign: 'center', alignItems: 'center', display:'contents'}}>  
             <Card.Body> 
